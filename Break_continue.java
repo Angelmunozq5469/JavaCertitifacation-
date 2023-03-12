6bb4a547c5140b43cf4f357e933e2857077bfca8
AngelE <munozq.angel@uces.edu.co>
2023-03-12 13:30:55 -0500

    solo falta continues and break

---
+++ Break_continue.java
@@ -0,0 +1,9 @@
+/**
+ * Break_continue
+ */
+public class Break_continue {
+
+    public static void main(String[] args) {
+
+    }
+}
--- Loops_en_java.java
+++ Loops_en_java.java
@@ -13,4 +13,87 @@
  * 
  * La implementacion del loop while es muy similar a la de for para esto veremos como se escribe:
  * 
+ * int i = 10;
+ * while(i=>20){
+ * System.out.printnl("este es el valor de i " + i)
+ * i++;
+ * }
+ * 
+ * While por otra parte es mas recomendado utilizarlo para con bloques de True or false es decir que la condicion
+ * varia nos sabemos cuando una condicion es True o es False, es de suma importancia tener muy claras las condiciones
+ * dentro de un ciclo.
+ * 
+ * 
+ * En ultimo tenemos el ciclo do While que tiene una caracteristica muy especial que como su nombre lo dice 
+ * lo que hace es hacer y despues validar el while nos daremos cuenta por su estructura 
+ *  
+ * int i = 10;
+ * 
+ *      do{
+ * 
+ *      System.out.printl("tu valor es" + i);
+ *      i --;
+ *      } while (i<=10)
+ * 
  */
+
+ /**
+  * Loops_en_java
+  */
+ public class Loops_en_java {
+ 
+    public static void main(String[] args) {
+        for (int i = 1; i<=10; i++){
+            System.out.println("El valor de i es "  + " "  + i);
+        }
+
+        System.out.println("\n" + """
+            
+        
+        """);
+        
+
+        new Loops_en_do_while();
+
+        System.out.println("\n" + """
+            
+        
+        """);
+        new Loops_while();
+        
+
+    }
+        
+
+    public static class Loops_while{
+        int i = 1;
+
+        public Loops_while(){
+
+            while (i<=10) {
+                System.out.println("El valor de i tera " + " " + i);
+                i ++;
+                
+            }
+        
+
+        }
+
+
+        
+    }
+
+    public static class Loops_en_do_while {
+        double i = 1.0;
+        public Loops_en_do_while(){
+
+            do {
+                System.out.println("El valor de i pero " + " " + i);
+                i++;
+                
+            } while (i<=10.0);
+        }
+    
+        
+    }
+ }
