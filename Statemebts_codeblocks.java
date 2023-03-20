e198a148fc029ba89b3f1d2cd5ed3c02cc7c3258
AngelE <munozq.angel@uces.edu.co>
2023-03-19 21:30:31 -0500

    Statementsandcodeblocksonjava

---
+++ Statemebts_codeblocks.java
@@ -0,0 +1,23 @@
+/*
+Statements es una intruccion completa que se le da a el programa para que haga
+
+ */
+
+ /**
+  * Statemebts_codeblocks
+  */
+ public class Statemebts_codeblocks {
+ 
+    public static void main(String[] args) {
+        int fecha = 2018;
+        System.out.print(fecha);
+    }
+ }
+
+ /*
+  * Recordar que los espacio y las identaciones entre las lineas de codigo son de alta importancia
+  */
+
+  /*
+   * Integer Literals 
+   */
