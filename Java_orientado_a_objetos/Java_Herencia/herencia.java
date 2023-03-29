b3529670eb766f98932c5c38eca0e07cc4678b65
AngelE <munozq.angel@uces.edu.co>
2023-03-28 22:45:07 -0500

    herenciatypes

--- Java_orientado_a_objetos/Java_Herencia/herencia.java
+++ Java_orientado_a_objetos/Java_Herencia/herencia.java
@@ -3,4 +3,7 @@ package Java_orientado_a_objetos.Java_Herencia;
 public class herencia {
     
 }
-/*revisar este tema de herencia */
+/*En herencia hay varios tipos de Herencia simple, 
+ * Herencia gerarquica, herencia hibrida, herencia multinivel
+ *
+*/
