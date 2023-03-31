0fdc1611de487f338c91e6d1cf98200e84415bf8
AngelE <munozq.angel@uces.edu.co>
2023-03-30 22:45:16 -0500

    Polimorfismo

---
+++ Animal/Hello.java
@@ -0,0 +1,17 @@
+package Animal;
+
+public class Hello {
+
+
+public class Main {
+    public static void main(String[] args) {
+        Animal animal1 = new Dog();
+        Animal animal2 = new Cat();
+
+        animal1.makeSound(); // Output: "The dog barks"
+        animal2.makeSound(); // Output: "The cat meows"
+    }
+}
+
+    
+}
