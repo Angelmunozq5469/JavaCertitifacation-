1a50aa895f7ea46cf2b70d4ba69daa1dbededc07
AngelE <munozq.angel@uces.edu.co>
2023-03-31 23:31:30 -0500

    interfacesjava

---
+++ Interfaces/Circle.java
@@ -0,0 +1,18 @@
+package Interfaces;
+
+public class Circle implements Shape {
+    private double radius;
+    
+    public Circle(double radius) {
+       this.radius = radius;
+    }
+ 
+    public double getArea() {
+       return Math.PI * radius * radius;
+    }
+ 
+    public void draw() {
+       System.out.println("Drawing a circle");
+    }
+ }
+ 
---
+++ Interfaces/Shape.java
@@ -0,0 +1,6 @@
+package Interfaces;
+public interface Shape {
+    double getArea(); // an abstract method
+    void draw(); // an abstract method
+ }
+ 
