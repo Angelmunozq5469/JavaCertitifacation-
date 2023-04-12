6eae41e2d416a1bfb1efea30e0def6dc44c4eacf
AngelE <munozq.angel@uces.edu.co>
2023-04-11 22:41:50 -0500

    excepcionparacaptarelerroryseguirprocesandoelrequirimiento

---
+++ app.java
@@ -0,0 +1,31 @@
+public class app {
+    public static void main(String[] args) {
+        int x=0;
+
+        try {
+            
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+        catch{
+
+        }
+
+        
+    }
+}
+
+/*
+ * El manejo de excepciones en Java es una forma de
+ *  tratar los errores o situaciones 
+ * inesperadas que pueden ocurrir durante la ejecución de un programa. Cuando se produce un error, 
+ * se genera un objeto que contiene información sobre el error. Java proporciona una manera de atrapar
+ *  estos errores y manejarlos con gracia, para que el programa 
+ * pueda seguir ejecutándose sin bloquearse. Para ello se utiliza el bloque try-catch-finally. El bloque 
+ * try contiene el código que podría causar un error, el bloque catch atrapa el error y lo maneja, y el bloque 
+ * finally contiene código que siempre se ejecuta, independientemente de si se ha producido un error o no.
+
+
+ * 
+ * 
+ */
