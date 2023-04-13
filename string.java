e463adc45ac4d402071611fceaa38e29e7d82c8a
AngelE <munozq.angel@uces.edu.co>
2023-04-12 23:38:34 -0500

    stringconquals

---
+++ string.java
@@ -0,0 +1,15 @@
+public class string {
+    public static void main(String[] args) {
+        String x= "S";
+        String y = "A";
+        String z = x + y;
+        if (z.equals("SA")){
+            System.out.println("x mas y es igual a SA");
+
+        }else{
+            System.out.print("z es otra cosa diferente a SA");
+        }
+
+
+    }
+}
