6817ef4128accbc38f324138d9ecd287471c83ef
AngelE <munozq.angel@uces.edu.co>
2023-04-13 22:45:36 -0500

    throw

---
+++ throwe.java
@@ -0,0 +1,11 @@
+public class throwe {
+    
+
+    public static void main(String[] args) {
+        int denominator =0;
+        if (denominator == 0) {
+            throw new ArithmeticException("Division by zero");
+        }
+        
+    }
+}
