e7a4fa04a8c13a66743e2a514340e4cbb1d3aa35
AngelE <munozq.angel@uces.edu.co>
2023-04-18 21:47:58 -0500

    forconArray

--- Array/array.java
+++ Array/array.java
@@ -4,7 +4,20 @@ public class array {
     public static void main(String[] args) {
         int [] intvatiable = {1,2,3,4,5,6,7,8,9,10};
         System.out.print(intvatiable[1]);
-        
+    }
 
+        class Innerarray {
+
+            String[] arrayStrings = {"Hola","como estas","Qumeas"};
+            for(int i=0;i<=3;i++)
+                {
+                    System.out.println(i);
+                }
+
+        
+            
+        }
     }
 }
+
+
