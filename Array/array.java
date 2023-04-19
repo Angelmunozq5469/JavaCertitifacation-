a9fbddaaac7c8b0737e8e7ff1c77f28df34d2820
AngelE <munozq.angel@uces.edu.co>
2023-04-18 21:55:01 -0500

    JavaloopArraywithloopforeach

--- Array/array.java
+++ Array/array.java
@@ -4,20 +4,25 @@ public class array {
     public static void main(String[] args) {
         int [] intvatiable = {1,2,3,4,5,6,7,8,9,10};
         System.out.print(intvatiable[1]);
+        System.out.println(new arraywithloop());
     }
 
-        class Innerarray {
+    public static class arraywithloop {
 
-            String[] arrayStrings = {"Hola","como estas","Qumeas"};
-            for(int i=0;i<=3;i++)
-                {
-                    System.out.println(i);
-                }
+        public arraywithloop (){
 
-        
-            
+            String [] name = {"Angel","Kelly","Felipe","Luz Dary","Miguel"};
+
+            for(int i = 0; i<=4;i++){
+                System.out.println(name[i]);
+            }
+
+
+
         }
+        
     }
+        
 }
 
 
