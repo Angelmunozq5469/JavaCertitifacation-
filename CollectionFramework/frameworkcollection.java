404008b6b4fd26fa92e680bede8523bc6752b867
AngelE <munozq.angel@uces.edu.co>
2023-04-18 22:55:45 -0500

    frameworkcolleection

---
+++ CollectionFramework/frameworkcollection.java
@@ -0,0 +1,14 @@
+package CollectionFramework
+
+
+public class frameworkcollection{
+    public static void main(String[] args) {
+        
+    }
+}
+
+
+/*
+ * Framework collection hace referencia a la eficencia de la estructura de datos dentro
+ * de un programa
+ */
