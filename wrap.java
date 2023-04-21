42ad1f7df6381407da85c9c4726a740377a11064
AngelE <munozq.angel@uces.edu.co>
2023-04-20 23:08:17 -0500

    wraper

---
+++ wrap.java
@@ -0,0 +1,11 @@
+public class wrap {
+    public static void main(String[] args) {
+        
+        /*
+         * El wrap que se hace es con un integer 
+         * este integer nos ayuda a convertir int to string o viceversa 
+         * este es necesario cuando estamos manipulando datos y cambiandolos porque podemos definir si necesitamos en ese
+         * momento un int o string
+         */
+    }
+}
