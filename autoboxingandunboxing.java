0b48da38d669176e82cbf6db7e579a7781da1c73
AngelE <munozq.angel@uces.edu.co>
2023-05-02 21:29:46 -0500

    ubbnboxing

--- autoboxingandunboxing.java
+++ autoboxingandunboxing.java
@@ -1,8 +1,15 @@
 public class autoboxingandunboxing {
+
+int i = 10;
+Integer wrapper = i; // autoboxing
+int j = wrapper; // unboxing
+
     
 }
 
 /*
- * Autoboxing in wrapper class es la conversion automatica mediada por el copilador de Java   que se hace entre los 
- * datos primitivos si la conversion es manual se le llama unboxing
+ * In summary, autoboxing converts a primitive type to its corresponding wrapper class 
+ * type, and unboxing converts an object of a wrapper class type to its corresponding primitive type. 
+ * Both conversions occur automatically at compile time and
+ *  do not require any explicit action from the programmer.
  */
