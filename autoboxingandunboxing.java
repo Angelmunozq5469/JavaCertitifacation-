14ee290ad2f3066433dddf93449b841d686814e5
AngelE <munozq.angel@uces.edu.co>
2023-05-02 21:23:19 -0500

    wrapper unboxing and autoboxing

---
+++ autoboxingandunboxing.java
@@ -0,0 +1,8 @@
+public class autoboxingandunboxing {
+    
+}
+
+/*
+ * Autoboxing in wrapper class es la conversion automatica mediada por el copilador de Java   que se hace entre los 
+ * datos primitivos si la conversion es manual se le llama unboxing
+ */
