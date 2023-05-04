388bb87a9498fbd468f949395133035df789f8f3
AngelE <munozq.angel@uces.edu.co>
2023-05-03 22:55:43 -0500

    generincjava

---
+++ generic.java
@@ -0,0 +1,12 @@
+public class generic {
+    
+}
+
+
+/*
+ * In Java, a generic is a type of class, interface, or method that can be 
+ * parameterized with one or more types. Generics were introduced in Java 5 to enhance the
+ *  type-safety of collections and to provide a way to write more reusable and type-safe code.
+
+For example, instead of declaring a collection of objects like this:
+ */
