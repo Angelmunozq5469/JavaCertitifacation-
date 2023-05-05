369a40a80ff20ef9c8a8cd6eab77688cb26e2205
AngelE <munozq.angel@uces.edu.co>
2023-05-04 22:32:54 -0500

    Typeparametresinjava

---
+++ typeparamete.java
@@ -0,0 +1,18 @@
+public class typeparamete <T> {  /*Este es con la finaliad de castear que va a retornar la funcion puesto que indicamos
+    el tipo de dato */
+    class implementation
+    
+}
+
+
+/*
+ *In Java, type parameters, also known as generics, allow you to define a class, method, or interface that can work with different data types. 
+ Type parameters are placeholders for actual types that will be determined at the time of the object's creation or
+  method's invocation.
+
+Type parameters are defined using angle brackets (<>) and are placed after the class name, method name, 
+or interface name. For example, to define a generic class that works with a specific type, 
+you can use the following syntax: 
+ * 
+ * 
+ */
