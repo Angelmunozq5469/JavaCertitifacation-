9be9c0ab69699b51533dbf0c108b7628085eb1fd
AngelE <munozq.angel@uces.edu.co>
2023-05-05 20:08:20 -0500

    sortingjava

---
+++ sorting.java
@@ -0,0 +1,17 @@
+import java.util.Set;
+import java.util.TreeSet;
+
+import javax.xml.crypto.Data;
+
+public class sorting {
+    public static void main(String[] args) {
+        Set<Data> set = new TreeSet<>();
+
+        set.add(new Data<Integer, String>(1,"chand"))
+
+        /*
+         * sorting es para organizar de acuerdo a lo que necesitemos tambien podemos
+         * hacer sorting con comparaciones
+         */
+    }
+}
