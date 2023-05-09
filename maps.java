69744499cb5c9688fdd56036da363f228901454c
AngelE <munozq.angel@uces.edu.co>
2023-05-08 23:24:01 -0500

    mapstre

--- maps.java
+++ maps.java
@@ -3,3 +3,14 @@ public class maps {
         
     }
 }
+/*
+ * Java provides several different implementations of the Map interface, each with its own characteristics and performance trade-offs. Here are some of the most commonly used implementations:
+
+HashMap: This is the most commonly used implementation of the Map interface. 
+It provides constant-time performance for the basic operations (put and get) and is very 
+efficient for large datasets. 
+However, it does not maintain any order for its elements.
+TreeMap: This implementation maintains the elements in sorted order based 
+on the natural order of their keys (or a custom comparator, if provided).
+ However, its performance is slower than that of the HashMap for most operations.
+ */
