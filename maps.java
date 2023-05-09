5b93878dc5b0591b94e02dd7c47aeb33c88bfa86
AngelE <munozq.angel@uces.edu.co>
2023-05-08 23:24:42 -0500

    maps

--- maps.java
+++ maps.java
@@ -13,4 +13,9 @@ However, it does not maintain any order for its elements.
 TreeMap: This implementation maintains the elements in sorted order based 
 on the natural order of their keys (or a custom comparator, if provided).
  However, its performance is slower than that of the HashMap for most operations.
+
+ LinkedHashMap: This implementation maintains a predictable iteration 
+ order based on the order in which the elements were added to the map. 
+ It also provides constant-time performance for the basic operations, 
+ although its performance is slightly slower than that of the HashMap.
  */
