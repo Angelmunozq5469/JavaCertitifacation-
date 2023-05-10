7a920c3adadaff7ceb1280657d40d78f3a87834f
AngelE <munozq.angel@uces.edu.co>
2023-05-09 23:02:12 -0500

    Reorganizacion

--- multithreading/multithreading.java
+++ multithreading/multithreading.java
@@ -5,15 +5,26 @@ public class multithreading {
      * Hay varios tipos de metodos de sincronizacion con la finalidad de prevenir que solo
      * un proceso ingrese a las variables de una clase, en caso tal de un Banco por ejemplo.
      * 
-     * In Java, synchronized methods are used to provide thread safety in a multi-threaded environment. When a method is declared as synchronized, it means that only one thread can access that method at a time. This prevents multiple threads from 
+     * In Java, synchronized methods are used to provide thread safety
+     *  in a multi-threaded environment. When a method is declared as synchronized, 
+     * it means that only one thread can access that method at a time. This prevents 
+     * multiple threads from 
      * accessing the same shared resource simultaneously and avoids race conditions and other concurrency issues.
 
-There are two types of synchronized methods in Java: instance synchronized methods and static synchronized methods.
+There are two types of synchronized methods in Java: instance synchronized 
+methods and static synchronized methods.
 
 Instance Synchronized Method:
 Instance synchronized methods are synchronized on 
-the instance of the class. This means that when a thread calls an instance synchronized method, it acquires a lock on the instance of the object and prevents any other thread from accessing any other synchronized instance method of that object until the lock is released.
+the instance of the class. This means that when a thread calls an instance
+ synchronized method, it acquires a lock on the instance of the object and prevents 
+ any other thread from accessing any other synchronized instance method of that object 
+ 
+ until the lock is released.
 Static Synchronized Method:
-Static synchronized methods are synchronized on the class itself, not on the instance of the class. This means that when a thread calls a static synchronized method, it acquires a lock on the class and prevents any other thread from accessing any other static synchronized method of that class until the lock is released.
+Static synchronized methods are synchronized on the class itself, not on the instance of 
+the class. This means that when a thread calls a static synchronized method, it acquires a 
+lock on the class and prevents any other thread from accessing any other static synchronized 
+method of that class until the lock is released.
      */
 }
