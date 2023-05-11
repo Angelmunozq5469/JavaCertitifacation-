5235231b1057ffbda7c387b8489fa73c32ea2994
AngelE <munozq.angel@uces.edu.co>
2023-05-10 21:46:08 -0500

    programa

---
+++ multithreading/App.java
@@ -0,0 +1,33 @@
+package multithreading;
+
+public class App {
+    package org.studyeasy;
+
+
+class MyCounter extends Thread{
+	private int threadNo;
+
+	public MyCounter(int threadNo) {
+		this.threadNo = threadNo;
+	}
+	
+	@Override
+	public void run() {
+		countMe();
+	}
+	
+	public void countMe(){
+		for(int i=1; i<=9;i++){
+			try {
+				sleep(500);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			System.out.println("The value if i is: "+i+" and the thread number is: "+threadNo);
+		}
+	}
+	
+}
+
+}
