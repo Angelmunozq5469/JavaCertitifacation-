c43a3c0795e17a20f37106dd23aa6811b2207c87
AngelE <munozq.angel@uces.edu.co>
2023-05-16 20:55:23 -0500

    taskinfila

--- multithreading/Explicaciondehilos.java
+++ multithreading/Explicaciondehilos.java
@@ -4,6 +4,6 @@ public class Explicaciondehilos {
     /*debemos de conocer los hilos en java porque estos nos permite hacer un codigo 
      * amplio y con mucho trabajos ya que los hilos en java son tareas que tiene el programa 
      * por ejemplo podemos hacer tareas sincronicas, asincronicas, tareas que necesiten estar en una
-     * fila segun la dependencia que tengan de otra tarea
+     * fila es decir se ejecuta una sigue la otra, 
      */
 }
