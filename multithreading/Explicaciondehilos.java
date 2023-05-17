fc10b6ddabbbc0c6a306fdebc3eb3746693ff0f8
AngelE <munozq.angel@uces.edu.co>
2023-05-16 20:53:05 -0500

    task

---
+++ multithreading/Explicaciondehilos.java
@@ -0,0 +1,9 @@
+package multithreading;
+
+public class Explicaciondehilos {
+    /*debemos de conocer los hilos en java porque estos nos permite hacer un codigo 
+     * amplio y con mucho trabajos ya que los hilos en java son tareas que tiene el programa 
+     * por ejemplo podemos hacer tareas sincronicas, asincronicas, tareas que necesiten estar en una
+     * fila segun la dependencia que tengan de otra tarea
+     */
+}
