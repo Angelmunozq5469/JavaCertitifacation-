3b52c4ea881af403738c66a8872ea32cce802b0f
AngelE <munozq.angel@uces.edu.co>
2023-05-16 21:06:01 -0500

    taskliberiasparamultithreading

--- multithreading/Explicaciondehilos.java
+++ multithreading/Explicaciondehilos.java
@@ -5,5 +5,6 @@ public class Explicaciondehilos {
      * amplio y con mucho trabajos ya que los hilos en java son tareas que tiene el programa 
      * por ejemplo podemos hacer tareas sincronicas, asincronicas, tareas que necesiten estar en una
      * fila es decir se ejecuta una sigue la otra, por otro lado en caso tal de que una tarea necesite 
-     * el recurso de otra. por ejemplo estas son algunas de las funciones que se utilizan. R
+     * el recurso de otra. por ejemplo estas son algunas de las funciones que se utilizan. 
+     * Reentrant lock, Deadlock, Blocking Queue, CountDownLatch, Thread pools. 
 }
