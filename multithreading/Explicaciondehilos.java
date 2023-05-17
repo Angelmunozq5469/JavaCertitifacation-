320144505984f048cd5f2d5e57c94307bc78479a
AngelE <munozq.angel@uces.edu.co>
2023-05-16 21:02:14 -0500

    librerias

--- multithreading/Explicaciondehilos.java
+++ multithreading/Explicaciondehilos.java
@@ -4,6 +4,6 @@ public class Explicaciondehilos {
     /*debemos de conocer los hilos en java porque estos nos permite hacer un codigo 
      * amplio y con mucho trabajos ya que los hilos en java son tareas que tiene el programa 
      * por ejemplo podemos hacer tareas sincronicas, asincronicas, tareas que necesiten estar en una
-     * fila es decir se ejecuta una sigue la otra, 
-     */
+     * fila es decir se ejecuta una sigue la otra, por otro lado en caso tal de que una tarea necesite 
+     * el recurso de otra. por ejemplo estas son algunas de las funciones que se utilizan. R
 }
