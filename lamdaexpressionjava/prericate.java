c7c138e8a611d36ebcdd9f041a46c275d9e73821
AngelE <munozq.angel@uces.edu.co>
2023-05-18 22:32:19 -0500

    Predicate

--- lamdaexpressionjava/prericate.java
+++ lamdaexpressionjava/prericate.java
@@ -2,11 +2,21 @@ package lamdaexpressionjava;
 
 import java.util.function.IntPredicate;
 
+import output.app;
+
 public class prericate {
     public static void main(String[] args) {
         IntPredicate lessThan18 = i->i<18;
+        IntPredicate MoreThan10 = i->i>10;
 
-        System.out.println(lessThan18.test(12));
-        
+        new prericate().demo(11,lessThan18,MoreThan10);
+    }
+
+    public void demo(int x, IntPredicate lessThan18, IntPredicate MoreThan10){
+        if(lessThan18.and(MoreThan10).test(x)){
+            System.out.println("The input is less than 18 and more than 10");
+        }else{
+            System.out.println("El input es Invalido");
+        }
     }
 }
