30ffa731e8f5c5c5e03e54ac7191a34c76c5badf
AngelE <munozq.angel@uces.edu.co>
2023-05-23 18:25:03 -0500

    estedocumentosirveparaguardararvhivosconjava

--- FileHandlingonjava/creatingfileondisk.java
+++ FileHandlingonjava/creatingfileondisk.java
@@ -1,5 +1,15 @@
 package FileHandlingonjava;
+import java.io.File;
+import java.io.IOException;
 
 public class creatingfileondisk {
-    
+    public static void main(String[] args) throws IOException {
+
+        {
+            File file = new File("nul","null");
+            file.createNewFile();
+            System.out.println("File Created");
+        }
+        
+    }
 }
