667dc1dddffd2be847d2d6be2f61d6bf121f6a7b
AngelE <munozq.angel@uces.edu.co>
2023-05-23 18:36:03 -0500

    directorio

---
+++ FileHandlingonjava/creatingadirectori.java
@@ -0,0 +1,15 @@
+package FileHandlingonjava;
+
+import java.io.File;
+
+public interface creatingadirectori {
+
+    public static void main(String[] args)  {
+        {
+            File directory = new File("study");
+            directory.mkdir();
+
+        }
+    }
+    
+}
