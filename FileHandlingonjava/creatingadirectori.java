afd069c37caaab638dc116dbbb195f8c2ff68e40
AngelE <munozq.angel@uces.edu.co>
2023-05-23 19:09:26 -0500

    howusejavafirwriterindirectoryfileintothatdirectory

--- FileHandlingonjava/creatingadirectori.java
+++ FileHandlingonjava/creatingadirectori.java
@@ -1,13 +1,28 @@
 package FileHandlingonjava;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.Buffer;
 
 public interface creatingadirectori {
 
-    public static void main(String[] args)  {
+    public static void main(String[] args) throws IOException  {
         {
             File directory = new File("study");
-            directory.mkdir();
+            directory.mkdirs();
+            File file = new File("nul","null");
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            System.out.println("File Created");
+            System.out.println("Dire");
+
+            BufferedWriter bw = new BufferedWriter(new FileWriter(file));
 
         }
     }
