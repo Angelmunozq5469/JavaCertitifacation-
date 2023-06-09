9416c6fe466ecd034702f1cf5e078bd3571a40a1
AngelE <munozq.angel@uces.edu.co>
2023-06-09 18:56:00 -0500

    intentandocambiarelservletfileparacorrerpestañas

---
+++ EclipseProyect/Webapp/src/main/java/server/Controller.java
@@ -0,0 +1,20 @@
+package server;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/Controller")
+public class Controller extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.sendRedirect(request.getContextPath() + "/login.jsp");
+    }
+}
+
