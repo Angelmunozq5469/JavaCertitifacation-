ae144cf2bdafff4ab71693d68bb5e06a64330c4c
AngelE <munozq.angel@uces.edu.co>
2023-06-28 21:56:05 -0500

    settingdb

--- EclipseProyect/Webapp/src/main/java/server/conect_db.java
+++ EclipseProyect/Webapp/src/main/java/server/conect_db.java
@@ -1,6 +1,8 @@
 package server;
 
 import java.io.IOException;
+
+import javax.annotation.Resource;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,13 +13,9 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/conect_db")
 public class conect_db extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private Datasource dataSource;
+	@Resource(name="jdbc/project")
        
-
-    public conect_db() {
-        super();
-
-    }
-
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
