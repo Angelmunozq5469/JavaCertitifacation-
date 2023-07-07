25651a01ece8f72182080d9fa77d61747d67105e
AngelE <munozq.angel@uces.edu.co>
2023-07-06 21:56:33 -0500

    inicializandobjetosconimportsqlquefuncionaparajava

--- EclipseProyect/Webapp/src/main/java/server/conect_db.java
+++ EclipseProyect/Webapp/src/main/java/server/conect_db.java
@@ -1,6 +1,8 @@
 package server;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
 
 import javax.annotation.Resource;
 import javax.servlet.ServletException;
@@ -9,6 +11,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.sql.DataSource;
+
+import com.mysql.cj.xdevapi.Statement;
 
 @WebServlet("/conect_db")
 public class conect_db extends HttpServlet {
@@ -19,8 +23,10 @@ public class conect_db extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         // Use the dataSource object to establish a connection with the database
         // 1. inicializar objetos de inicio
-    	// 2. Create a SQL statements string
-    	// 3. Execute SQL query
-    	// 4. Process the result set
+    	Connection connect = null;
+    	Statement stmt = null;
+    	ResultSet rs = null;
+    	
+    	
     }
 }
