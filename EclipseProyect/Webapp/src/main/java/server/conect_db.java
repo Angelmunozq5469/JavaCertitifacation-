144089826c93c22eaacffda2c59bf0ac9f4dc8ec
AngelE <munozq.angel@uces.edu.co>
2023-07-06 21:34:03 -0500

    pasosparacrearconexiontodatabase

--- EclipseProyect/Webapp/src/main/java/server/conect_db.java
+++ EclipseProyect/Webapp/src/main/java/server/conect_db.java
@@ -19,5 +19,8 @@ public class conect_db extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         // Use the dataSource object to establish a connection with the database
         // 1. inicializar objetos de inicio
+    	// 2. Create a SQL statements string
+    	// 3. Execute SQL query
+    	// 4. Process the result set
     }
 }
