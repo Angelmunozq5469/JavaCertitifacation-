a9aa35ff55e0d61a5b95f56e7fc94caf1db74677
AngelE <munozq.angel@uces.edu.co>
2023-07-12 22:15:22 -0500

    pasandocontrollerparadata

--- EclipseProyect/Webapp/src/main/java/Model/usermodel.java
+++ EclipseProyect/Webapp/src/main/java/Model/usermodel.java
@@ -1,5 +1,32 @@
 package Model;
 
-public class usermodel {
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
+public class usermodel {
+	public listuser() {
+		PrintWriter out = response.getWriter();
+    	Connection connect = null;
+    	Statement stmt = null;
+    	ResultSet rs = null;
+    	try {
+    	  connect = dataSource.getConnection();
+    	// 2. Create a SQL statements string
+    	  String query = "Select * from User";
+    	  stmt = (Statement) connect.createStatement();
+    	  
+      	// 3. Execute SQL query
+    	  rs = ((java.sql.Statement) stmt).executeQuery(query);
+      	// 4. Process the result set
+    	  while(rs.next()) {
+    		  out.print(rs.getString("email"));    		  
+    	  }
+    		
+    	} catch (SQLException e) {
+    		e.printStackTrace();
+    	}
+	}
 }
