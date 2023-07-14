65ba59939563ef97304bef099998fc90ab3e0cb3
AngelE <munozq.angel@uces.edu.co>
2023-07-13 23:07:34 -0500

    solucionandoerrorescondatadisplay

---
+++ EclipseProyect/Webapp/src/main/java/controladoresdehome.java
@@ -0,0 +1,44 @@
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.Resource;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.apache.tomcat.jdbc.pool.DataSource;
+
+import entity.user;
+
+@WebServlet("/home")
+public class controladoresdehome extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+    @Resource(name="jdbc/project")
+    private DataSource dataSource;
+    
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        // Handle GET request
+        String page = request.getParameter("page");
+        page = page.toLowerCase();
+        
+        switch(page) {
+            case "clientes":
+                List<user> listuser = new ArrayList<>();
+                listuser = new usermodel().listuser(dataSource);
+                request.setAttribute("listuser", listuser);
+                request.getRequestDispatcher("clientes.jsp").forward(request, response);
+                break;
+                
+            case "sobre":
+                request.getRequestDispatcher("sobre.jsp").forward(request, response);
+                break;
+                
+            default:
+                request.getRequestDispatcher("index.jsp").forward(request, response);
+        }
+    }
+}
--- EclipseProyect/Webapp/src/main/webapp/clientes.jsp
+++ EclipseProyect/Webapp/src/main/webapp/clientes.jsp
@@ -1,11 +1,32 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ page import= "java.util.List" %>
+<%@ page import="entity.user" %>
 <jsp:include page="/index.jsp" ></jsp:include>
 
 <div class="container mtb">
     <div class="row">
         <div class="col-lg-6 d-flex flex-column align-items-center justify-content-center">
-            <h1 style="margin-bottom: 100px;">bienvenido</h1>
-            <p style="margin-top: 10px;">bienvenido</p>
+            <h1 style="margin-bottom: 100px;">Error page</h1>
+            <p style="margin-top: 10px;">Error page</p>
+            <strong>Listing users</strong>
+            <hr/>
+            <table border="1">
+                <thead>
+                    <th>UserID</th>
+                    <th>Username</th>
+                    <th>Email</th>
+                </thead>
+                <%
+                List<user> listuser = (List)request.getAttribute("listuser");
+                for (int i=0;i<listuser.size(); i++){
+                    out.print("<tr>");
+                    out.print("<td>"+listuser.get(i).getUser_ID()+"</td>");
+                    out.print("<td>"+listuser.get(i).getUsername()+"</td>");
+                    out.print("<td>"+listuser.get(i).getEmail()+"</td>");
+                    out.print("</tr>");
+                }
+                %>
+            </table>
         </div>
     </div>
 </div>
-
