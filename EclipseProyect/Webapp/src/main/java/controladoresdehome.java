3eeed20c1b23d0b7c5f08426f1873f79dcc177b9
AngelE <munozq.angel@uces.edu.co>
2023-07-13 23:27:57 -0500

    errornulcargandodatos

--- EclipseProyect/Webapp/src/main/java/controladoresdehome.java
+++ EclipseProyect/Webapp/src/main/java/controladoresdehome.java
@@ -1,22 +1,23 @@
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+
 import javax.annotation.Resource;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
+import javax.sql.DataSource;
 
-import org.apache.tomcat.jdbc.pool.DataSource;
-
+import Model.usermodel;
 import entity.user;
 
 @WebServlet("/home")
 public class controladoresdehome extends HttpServlet {
     private static final long serialVersionUID = 1L;
-    @Resource(name="jdbc/project")
+    
+    @Resource(name = "jdbc/project")
     private DataSource dataSource;
     
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
@@ -25,20 +26,18 @@ public class controladoresdehome extends HttpServlet {
         String page = request.getParameter("page");
         page = page.toLowerCase();
         
-        switch(page) {
+        switch (page) {
             case "clientes":
-                List<user> listuser = new ArrayList<>();
-                listuser = new usermodel().listuser(dataSource);
+                List<user> listuser = new usermodel().listuser(dataSource);
                 request.setAttribute("listuser", listuser);
                 request.getRequestDispatcher("clientes.jsp").forward(request, response);
                 break;
-                
             case "sobre":
                 request.getRequestDispatcher("sobre.jsp").forward(request, response);
                 break;
-                
             default:
                 request.getRequestDispatcher("index.jsp").forward(request, response);
+                break;
         }
     }
 }
--- EclipseProyect/Webapp/src/main/java/entity/user.java
+++ EclipseProyect/Webapp/src/main/java/entity/user.java
@@ -1,34 +1,38 @@
 package entity;
 
 public class user {
-	private int user_ID;
-	private String username;
-	private String email;
-	
-	public user (int user_ID,String username, String email) {
-		super();
-		this.user_ID = user_ID;
-		this.username = username;
-		this.email = email;
-	}
-	public int getUser_ID() {
-		return user_ID;
-	}
-	public void setUser_ID(int user_ID) {
-		this.user_ID = user_ID;
-	}
-	public String getUsername() {
-		return username;
-	}
-	public void setUsername(String username) {
-		this.username = username;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
+    private int user_ID;
+    private String username;
+    private String email;
 
-	
+    public user(int user_ID, String username, String email) {
+        super();
+        this.user_ID = user_ID;
+        this.username = username;
+        this.email = email;
+    }
+
+    public int getUser_ID() {
+        return user_ID;
+    }
+
+    public void setUser_ID(int user_ID) {
+        this.user_ID = user_ID;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
