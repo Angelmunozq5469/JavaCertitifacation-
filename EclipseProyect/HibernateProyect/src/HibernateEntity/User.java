07e7d9073deae3178727510520856e0ef22c2935
AngelE <munozq.angel@uces.edu.co>
2023-07-19 22:09:52 -0500

    utilizando@entitypackageforuserhibernate

--- EclipseProyect/HibernateProyect/src/HibernateEntity/User.java
+++ EclipseProyect/HibernateProyect/src/HibernateEntity/User.java
@@ -1,6 +1,13 @@
 package HibernateEntity;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+@Entity
+@Table(name="User")
 public class User {
-	
+	int user_ID;
+	String username;
+	String email;
+
 
 }
