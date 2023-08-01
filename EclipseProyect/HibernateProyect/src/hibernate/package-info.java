44d24437d7e46611cc70a63e1e5d519a6af568da
AngelE <munozq.angel@uces.edu.co>
2023-07-31 23:18:53 -0500

    explicationspring

--- EclipseProyect/HibernateProyect/src/hibernate/package-info.java
+++ EclipseProyect/HibernateProyect/src/hibernate/package-info.java
@@ -5,3 +5,6 @@ package hibernate;
 // Desarrollo Java
 
 //Que es Sprint 5?
+
+
+//Spring is an open-source application framework that provides comprehensive infrastructure support for developing Java applications. It was developed by Rod Johnson and released in 2003. The framework is designed to simplify the development of enterprise-level Java applications by promoting a modular and flexible architecture.
