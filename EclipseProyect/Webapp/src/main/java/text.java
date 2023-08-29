963f75d7685a8faa16ab13112bfb35b6c12f4241
AngelE <munozq.angel@uces.edu.co>
2023-08-28 21:31:17 -0500

    pasos_a_seguir_para_crear_proyecto_creando_dependencia

--- EclipseProyect/Webapp/src/main/java/text.java
+++ EclipseProyect/Webapp/src/main/java/text.java
@@ -1,2 +1,8 @@
 /*En este caso comenzaremos a utilizar spring #5 con la finalidad de aprender como inyectarle
- * dependencia a la app*/
+ * dependencia a la app
+ * 
+ * 1. Implementamos una interfaz con la tematica (car)
+ * 2. hacemos las clases que necetamos por cada caso
+ * unico en este caso (cars)
+ * 3.creamos el app donde llamaremos todos los metodo (main)s*/
+
